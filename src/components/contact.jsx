d0b5f68c111c34f53d065232aa8099ce36515330
Slayder <slayderreyescuellar@gmail.com>
2025-06-02 10:41:07 -0500

    the handleSubmit was remove

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,14 +13,7 @@ function contact() {
       .join("&");
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    });
-  }
+  
 
   return (
     <section id="contact" className="relative">
@@ -67,7 +60,7 @@ function contact() {
           method="POST"
           data-netlify="true"
           netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
+          onSubmit="submit"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             ¡Hire me!
