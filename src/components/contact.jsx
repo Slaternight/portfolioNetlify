949b06790575393811b83209e28db4ba2cd33ffc
Slayder <slayderreyescuellar@gmail.com>
2025-06-02 10:11:59 -0500

    test

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,16 +13,7 @@ function contact() {
       .join("&");
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
+
 
   return (
     <section id="contact" className="relative">
@@ -66,7 +57,6 @@ function contact() {
         <form
           method="POST" data-netlify="true"
           name="contact"
-          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
