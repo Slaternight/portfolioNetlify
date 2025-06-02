535c22d766fe7fd65e26374a676cf1becae166ae
Slayder <slayderreyescuellar@gmail.com>
2025-06-02 10:38:37 -0500

    message sent was delete

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -19,9 +19,7 @@ function contact() {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    });
   }
 
   return (
@@ -66,6 +64,9 @@ function contact() {
         <form
           netlify
           name="contact"
+          method="POST"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
