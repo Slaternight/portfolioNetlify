0aa665228007bbc4b58ace104c216e42253b0df9
Slayder <slayderreyescuellar@gmail.com>
2025-06-02 09:52:57 -0500

    prettier format

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -67,7 +67,8 @@ function contact() {
           netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             ¡Hire me!
           </h2>
@@ -103,7 +104,8 @@ function contact() {
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
+              className="leading-7 text-sm text-gray-400"
+            >
               And your message
             </label>
             <textarea
@@ -116,7 +118,8 @@ function contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
             Enviar
           </button>
         </form>
