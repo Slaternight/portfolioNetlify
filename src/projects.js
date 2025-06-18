f7ff12925d3a8b2066427a7bd3935fad899c9140
slaternight <slayderreyescuellar@gmail.com>
2025-06-18 17:07:29 -0500

    the tab function was add and other updates

---
+++ src/projects.js
@@ -0,0 +1,74 @@
+import esferosWeb from './assets/esferos.webm'
+import runksWeb from './assets/runks.webm'
+import lumenxWeb from './assets/lumenx.webm'
+import tiendaFliv from './assets/tiendafliv.webm'
+import flivWeb from './assets/fliv.webm'
+import catalogoWeb from './assets/catalogo.webm'
+import globalCyg from './assets/globalcyg.webm'
+
+
+export const projects = [
+    {
+      title: "Boton Promo",
+      subtitle: "Ecommerce Web",
+      description:
+        "Built a dynamic web app using javascript, bootstrap5, and liquid, styled for accessibility and responsive behavior.",
+      image: esferosWeb,
+      link: "https://esferos.com",
+      category: "Shopify & Liquid",
+    },
+    {
+      title: "Runks Electric",
+      subtitle: "Ecommerce Web",
+      description:
+        "Built a dynamic web app using php, wordpress, and elementor, styled for accessibility and responsive behavior.",
+      image: runksWeb,
+      link: "https://runks.com.co",
+      category: "Wordpress & Elementor Pro",
+    },
+    {
+      title: "Lumenx",
+      subtitle: "Ecommerce Web",
+      description:
+        "Built a dynamic web app using php, wordpress, and elementor, styled for accessibility and responsive behavior.",
+      image: lumenxWeb,
+      link: "https://lumenx.com.co",
+      category: "Wordpress & Elementor Pro",
+    },
+    {
+      title: "Tienda Industrias Fliv",
+      subtitle: "Ecommerce Web",
+      description:
+        "Built a dynamic web app using php, wordpress, woocomerce and elementor, styled for accessibility and responsive behavior.",
+      image: tiendaFliv,
+      link: "https://tienda.fliv.com",
+      category: "Wordpress & Elementor Pro",
+    },
+    {
+      title: "Industrias Fliv",
+      subtitle: "Web Site",
+      description:
+        "Built a dynamic web site using php, wordpress, and elementor, styled for accessibility and responsive behavior.",
+      image: flivWeb,
+      link: "https://fliv.com.co",
+      category: "Wordpress & Elementor Pro",
+    },
+    {
+      title: "Catalogo Espacial",
+      subtitle: "Web Site",
+      description:
+        "Built a dynamic web site using liquid, shopify, and api use, styled for accessibility and responsive behavior.",
+      image: catalogoWeb,
+      link: "https://catalogoespacial.com",
+      category: "Shopify & Liquid",
+    },
+    {
+      title: "Global CYG",
+      subtitle: "Tourism Web Site",
+      description:
+        "Built a dynamic web site using Wordpress and Elementor pro, styled for accessibility and responsive behavior.",
+      image: globalCyg,
+      link: "https://globalcyg.com",
+      category: "Wordpress & Elementor Pro",
+    },
+  ];
