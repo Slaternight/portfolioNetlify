a757723f48324d166c2b9250bf63342893dca4a8
slaternight <slayderreyescuellar@gmail.com>
2025-06-20 15:19:10 -0500

    the behance link was change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
         <div className="flex justify-center space-x-6 text-gray-700 text-xl">
           <a href="https://www.facebook.com/slayder.reyes" target="_blank"><FontAwesomeIcon icon={faFacebookF} className="hover:text-blue-600 transition text-2xl" /></a>
           <a href="https://x.com/slaydermanreyes" target="_blank"><FontAwesomeIcon icon={faTwitter} className="hover:text-sky-500 transition text-2xl" /></a>
-          <a href="https://www.behance.net/slayderreyes/projects" target="_blank"><FontAwesomeIcon icon={faBehance} className="hover:text-red-500 transition text-2xl" /></a>
+          <a href="https://www.behance.net/slaternight" target="_blank"><FontAwesomeIcon icon={faBehance} className="hover:text-red-500 transition text-2xl" /></a>
           <a href="https://www.instagram.com/slaternight/" target="_blank"><FontAwesomeIcon icon={faInstagram} className="hover:text-pink-500 transition text-2xl" /></a>
           <a href="https://www.linkedin.com/in/slayder-reyes-cuellar-2b7b801a0/" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} className="hover:text-blue-700 transition text-2xl" /></a>
           <a href="https://github.com/Slaternight" target="_blank"><FontAwesomeIcon icon={faGithub} className="hover:text-black transition text-2xl" /></a>
