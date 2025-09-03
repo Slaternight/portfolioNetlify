2c8757354274a09da1faffc3b5771f63378e022d
slaternight <slayderreyescuellar@gmail.com>
2025-09-03 15:57:14 -0500

    the form title was change

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -88,7 +88,7 @@ function contact() {
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            ¡Hire me!
+            ¡Let's to talk!
           </h2>
           <p className="leading-relaxed mb-5">
             I'd be happy to hear about your project and help you carry it out.
